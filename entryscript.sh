abac0d17d1df6e3dd2e54ed332593cc36567655a
Chris Sim <monkeymon@gmail.com>
2020-02-14 23:38:34 +0800

    echo printenv

--- entryscript.sh
+++ entryscript.sh
@@ -7,12 +7,12 @@ TIME=`/bin/date +%d-%m-%Y-%T`
 
 S3_BUCKET="${INPUT_S3_BUCKET}"
 S3_PATH="${INPUT_S3_PATH:=''}"
-S3_LOC="$S3_BUCKET/$S3_PATH"
-S3_DB_DUMPFILE="$INPUT_DB_NAME_$TIME.sql.gz"
+S3_LOC=$S3_BUCKET/$S3_PATH
+S3_DB_DUMPFILE="$DB_NAME_$TIME.sql.gz"
 DESTLATEST="latest.sql.gz"
 
 mysqldump -h $DB_HOST -u $DB_USERNAME -p$DB_PASSWORD --databases $DB_NAME | gzip > ./$S3_DB_DUMPFILE
-
+printenv
 # Upload tar to s3
 export AWS_ACCESS_KEY_ID=$INPUT_AWS_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET_KEY
